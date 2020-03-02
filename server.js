7430e2539a4b88fe5697dfb706017749c6eccfb3
chad Farrington <sandrock11@hotmail.com>
2020-03-02 13:07:25 -0500

    added client components, about and home

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
 
 //delcare & initilize port variable
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 //allow  express to start listening on port 5000 and display a message
 app.listen(PORT, () => {
