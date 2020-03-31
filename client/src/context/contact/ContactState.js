e1c707835f129432487409a11cd861196d665822
chad Farrington <sandrock11@hotmail.com>
2020-03-31 17:51:30 -0400

    contacts are cleared from database

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -98,8 +98,25 @@ const ContactState = props => {
   };
 
   //update contact
-  const updateContact = contact => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  const updateContact = async contact => {
+    //create headers to send data
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (error) {
+      dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
+    }
   };
 
   // Filter Contacts
