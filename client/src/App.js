800b4c1e754c69c83c440fbdad1f7b5c1004122b
chad Farrington <sandrock11@hotmail.com>
2020-03-31 14:22:03 -0400

    users can log out the home page is now protected

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Register from './components/auth/Register';
 import Alerts from './components/layout/Alerts';
 import Login from './components/auth/Login';
 import setAuthToken from './utils/setAuthToken';
+import PrivateRoute from './components/routing/PrivateRoute';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
@@ -28,7 +29,7 @@ const App = () => {
               <div className='container'>
                 <Alerts />
                 <Switch>
-                  <Route exact path='/' component={Home} />
+                  <PrivateRoute exact path='/' component={Home} />
                   <Route exact path='/about' component={About} />
                   <Route exact path='/register' component={Register} />
                   <Route exact path='/login' component={Login} />
